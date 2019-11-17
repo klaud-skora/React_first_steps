0000a1336f47dc13eed7e3ead4b632b29f3cfd62
Klaudia Skóra <klaud.skora@gmail.com>
2019-11-17 22:58:22 +0100

    Add columns to List component - module 15.4.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    backgroundImage: PropTypes.string,
+    children: PropTypes.node,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title}  backgroundImage={this.props.backgroundImage}/>
+        <div className={styles.description}>
+            {this.props.children}
+        </div>
+        <div className={styles.columns}>
+            <Column title={'Animations'} />
+            <Column title={'Photos'} />
+            <Column title={'Videos'} />
+        </div>
       </section>
     )
   }
